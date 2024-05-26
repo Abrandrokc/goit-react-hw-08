7d8c49b1e25bf44195ea3c3b5d9904204c3b5926
Abrandrokc <Abrandrokco@gmail.com>
2024-05-26 23:37:44 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
     return <div>
         <h1>Phone list app</h1>
-        <p>Its app create to save phone number.
-            Just <span> <Link to="/login">try</Link></span> </p>
+        <p>Its app create to save phone number.</p>
     </div>
 }
